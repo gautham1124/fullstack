4529c8d7b148977fed0379ee9d7aee52d36bd7e5
gautham1124 <gautham1124@outlook.com>
2023-08-03 14:43:45 +0530

    added inter js

---
+++ prac.js
@@ -0,0 +1,54 @@
+// var a = 120
+// console.log("trial:"+a)
+// {
+//     let a =10
+// }
+// console.log("error:"+a)
+
+// a = 15
+
+// if(a==0){
+//     console.log("the number is 0")
+// }
+
+// else if (a>0){
+//     console.log("number greater than 0")
+// }
+
+// else{
+// console.log("number less than 0")
+// }
+
+// let a = 0
+// console.log(a)
+// if(a === 0){
+//     console.log("the number is 0 adding 1 to make it greater than 0")
+//     a = a+1
+//     console.log("a = a + 1 ="+a)
+// }
+
+// else if (a > 0){
+//     console.log("the number is positive")
+// }
+
+// else{
+//     console.log("the number is negative")
+// }
+
+// // for loop
+// a = 5
+// for (i = 0; i < a; i ++){
+//     console.log(i)
+// }
+
+// while loop
+// a = 5
+// i = 0
+// while(i <= a){
+//     console.log(i)
+//     i ++
+// }
+
+array = [10,50]
+array.forEach(element => console.log(element))
+
